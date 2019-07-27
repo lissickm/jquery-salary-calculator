984c5bc44e7658cd9b797931e7867b32f865cc45
Matt Lissick <mattlissick@gmail.com>
2019-07-27 08:58:42 -0500

    inputs are clearing upon submit

--- script.js
+++ script.js
@@ -9,14 +9,23 @@ function readyNow() {
 
     // event listeners
     $('#submitButton').on('click', addInfoToTable);
+    $('#submitButton').on('click', clearInputs);
     
 }
 
 function addInfoToTable () {
+    // adds input values to table upon click
+
     console.log('clicked');
-    $('#employeeInformation').append(`<tr>` + `<td>` + $('#firstNameInput').val() + `</td>` + `<td>` + $('#lastNameInput').val() + `</td>` + `<td>` + $('#idInput').val() + `</td>` + `<td>` + $('#titleInput').val() + `</td>` + `<td>` + "$" + $('#annualSalaryInput').val() + `</td>`
-    
-    
-    
-    +`</tr>`)
+    $('#employeeInformation').append(`<tr>` + `<td>` + $('#firstNameInput').val() + `</td>` + `<td>` + $('#lastNameInput').val() + `</td>` + `<td>` + $('#idInput').val() + `</td>` + `<td>` + $('#titleInput').val() + `</td>` + `<td>` + "$" + $('#annualSalaryInput').val() + `</td>` + `<td>` + `<button>` + `</button>` + `</td>` + `</tr>`);
+    $('#firstNameInput').val(' ');
+}
+
+function clearInputs () {
+    console.log('input cleared');
+    $('#firstNameInput').val('');
+    $('#lastNameInput').val('');
+    $('#idInput').val('');
+    $('#titleInput').val('');
+    $('#annualSalaryInput').val('');     
 }
